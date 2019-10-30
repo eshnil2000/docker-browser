adfd0d2b6797eb15872a57ad57a2c613d7d83bb4
Ubuntu <ubuntu@ip-172-31-12-74.us-west-1.compute.internal>
2019-10-30 22:20:09 +0000

    added swagger

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 var serverPort=10000;
 var host='proxy.chainapp.live';
 //time in milliseconds
-var timeLimit=10000;
+var timeLimit=3600000;
 var dockerNetwork='nginx-proxy';
 var containerLaunch='jwilder/whoami';
+var containerLaunch='swaggerapi/swagger-editor';
+//var containerLaunch='nginxdemos/hello';
 var containerPort=8000;
+var containerPort=8090;
+var virtualPort= containerPort;
 //END SET VARIABLES//
 const nocache = require('nocache');
 const express = require('express');
@@ -53,9 +57,10 @@ app.get('/', (req, res) =>{
 	});
 	newhost=subhost.concat('.').concat(host);
 	var child = run(containerLaunch, xtend(opts,{net:dockerNetwork,
-         env:{VIRTUAL_HOST:newhost      },
+         env:{VIRTUAL_HOST:newhost,VIRTUAL_PORT:virtualPort      },
          expose:containerPort,
-        } ))
+	 ports:containerPort,
+         }))
 
 	child.on('spawn', containerSpawned)
 	child.on('exit', containerExited)
