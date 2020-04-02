a6fd388d733ca480e7bc923d138937b8bcc6b6a3
root <root@ip-172-31-12-74.us-west-1.compute.internal>
2020-04-02 15:21:30 +0000

    server.js

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 var serverPort=10000;
 var host='proxy.chainapp.live';
 //time in milliseconds
-var timeLimit=3600000;
+var timeLimit=1000000;
 var dockerNetwork='nginx-proxy';
 var containerLaunch='jwilder/whoami';
-var containerLaunch='swaggerapi/swagger-editor';
+var containerLaunch='eshnil2000/dl-blockchain';
+var containerLaunch='eshnil2000/docker-ubuntu-vnc-desktop';
+
+//var containerLaunch='swaggerapi/swagger-editor';
 //var containerLaunch='nginxdemos/hello';
-var containerPort=8000;
-var containerPort=8090;
+var containerPort=80;
+var containerPort=80;
 var virtualPort= containerPort;
 //END SET VARIABLES//
 const nocache = require('nocache');
@@ -57,7 +60,7 @@ app.get('/', (req, res) =>{
 	});
 	newhost=subhost.concat('.').concat(host);
 	var child = run(containerLaunch, xtend(opts,{net:dockerNetwork,
-         env:{VIRTUAL_HOST:newhost,VIRTUAL_PORT:virtualPort      },
+         env:{ VIRTUAL_HOST:newhost,VIRTUAL_PORT:virtualPort      },
          expose:containerPort,
 	 ports:containerPort,
          }))
